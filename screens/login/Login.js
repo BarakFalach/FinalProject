98c15de8dc5e0d07d0f2efb4513fe500e3ac173b
barak falach <barak8995@gmail.com>
2022-06-23 19:10:51 +0300

    Naviagation

---
+++ screens/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native';
+
+function LoginScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
