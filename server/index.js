891483275ce2875510b7e9d6e45465a1f2fb3c2a
barak falach <barak8995@gmail.com>
2022-11-08 10:13:24 +0200

    db connected + example

--- server/index.js
+++ server/index.js
@@ -2,41 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios');
+const connectDb = require('./db/db');
+const User = require('./db/models/User');
+const bodyParser = require('body-parser');
+const { application } = require('express');
 
-const TOKEN =
-  'ya29.a0Aa4xrXPE6ZKwxhmGZ_MQY3EnrrqKA_7evGxoWviXeBHctvaasdH2w-K2VuPUeKaLffcjBnYTfnPWQ2-qkC2xjh6Y6hlRtuJ3_lpNtosyn9lopuJaKo37rOkbK1vTb7my9QnZom3LCyUG4cNRmat_6aMzAUATaCgYKATASARISFQEjDvL9GhGRO-xJi7qaHunf9EESeg0163';
+connectDb();
+app.use(bodyParser.json({ extended: false }));
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+//inset to db example
+app.post('/user', (req, res) => {
+  const { full_name } = req.body
+  const newUser = new User({ full_name })
+  newUser.save();
+  res.send('user Added')
+})
+
+//get All Users from db
+app.get('/user', (req, res) => {
+  User.find().then(users => res.json(users))
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-const stepCounter = async () => {
-  const result = await axios({
-    method: 'POST',
-    headers: {
-      authorization: 'Bearer ' + TOKEN,
-    },
-    'Content-Type': 'application/json',
-    url: `https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate`,
-    data: {
-      aggregateBy: [
-        {
-          dataTypeName: 'com.google.step_count',
-          dataSourceId:
-            'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
-        },
-      ],
-      bucketByTime: { durationMillis: 86400000 },
-      startTimeMillis: 1665683185085-86400000,
-      endTimeMillis: 1665683185085,
-    },
-  });
-  console.log('f')
-  console.log(JSON.stringify(result.data.bucket[0].dataset[0].point[0].value[0].intVal));
-}
-
-stepCounter()
 
