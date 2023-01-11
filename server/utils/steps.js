bfd055dfee3cac95486bac9d94f2841b8a3f365e
avichai edri <avichaie@post.bgu.ac.il>
2023-01-11 09:10:03 +0200

    make abstract step count func

--- server/utils/steps.js
+++ server/utils/steps.js
@@ -2,34 +2,16 @@
 const getTodayStepCount = async (TOKEN) => {
     const ms_now = Date.now();
     const ms_today = new Date().setHours(0, 0, 0, 0);
-
-    const result = await axios({
-        method: 'POST',
-        headers: {
-        authorization: 'Bearer ' + TOKEN,
-        },
-        'Content-Type': 'routerlication/json',
-        url: `https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate`,
-        data: {
-        aggregateBy: [
-            {
-            dataTypeName: 'com.google.step_score',
-            dataSourceId:
-                'derived:com.google.step_score.delta:com.google.android.gms:estimated_steps',
-            },
-        ],
-        bucketByTime: { durationMillis: 86400000 },
-        startTimeMillis: ms_today,
-        endTimeMillis: ms_now,
-        },
-    });
-        return JSON.stringify(result.data.bucket[0].dataset[0].point[0].value[0].intVal)
+    return await getStepsBetweenDate(TOKEN, ms_today, ms_now);
     
 } 
 const getStepsFromDate = async (TOKEN, date) => {
     const ms_now = Date.now();
     const ms_date = new Date(date).setHours(0, 0, 0, 0);
+    return await getStepsBetweenDate(TOKEN, ms_date, ms_now);
+};
 
+const getStepsBetweenDate = async (TOKEN, date_from, date_until) => {
     const result = await axios({
         method: 'POST',
         headers: {
@@ -46,12 +28,12 @@ const getStepsFromDate = async (TOKEN, date) => {
             },
         ],
         bucketByTime: { durationMillis: 86400000 },
-        startTimeMillis: ms_date,
-        endTimeMillis: ms_now,
+        startTimeMillis: date_from,
+        endTimeMillis: date_until,
         },
     });
         return JSON.stringify(result.data.bucket[0].dataset[0].point[0].value[0].intVal)
-}; //TODO:: implement this function
+};
 
 export default { getTodayStepCount, getStepsFromDate };
 
