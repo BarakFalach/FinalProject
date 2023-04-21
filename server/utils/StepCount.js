65932e1f34e74bdafe73f00d33149adbfff6d71b
barak falach <barak8995@gmail.com>
2023-04-21 11:12:17 +0300

    remove notes

--- server/utils/StepCount.js
+++ server/utils/StepCount.js
@@ -17,44 +17,43 @@ const GROUP_CODE = '7371'
 
 //TODO:: needs to add a refresh token logic
 const initStepCountHistory = async (email, TOKEN) => {
-  // const endDate = INIT_DATE
-  // const today = new Date();
-  // const yesterday = new Date(today.setDate(today.getDate() - 1));
-  // let score = 0
+  const endDate = INIT_DATE
+  const today = new Date();
+  const yesterday = new Date(today.setDate(today.getDate() - 1));
+  let score = 0
 
-  // for (let d = new Date(yesterday); d >= endDate; d.setDate(d.getDate() - 1)) {
+  for (let d = new Date(yesterday); d >= endDate; d.setDate(d.getDate() - 1)) {
     
-  //   let startTimeMillis = new Date(d);
-  //   let endTimeMillis = new Date(d);
-  //   startTimeMillis.setHours(0, 0, 0, 0);
-  //   endTimeMillis.setHours(23, 59, 59, 999);
+    let startTimeMillis = new Date(d);
+    let endTimeMillis = new Date(d);
+    startTimeMillis.setHours(0, 0, 0, 0);
+    endTimeMillis.setHours(23, 59, 59, 999);
 
-  //   console.log('Before StepCount', startTimeMillis, endTimeMillis);
-  //   console.log('looped date', d);
+    console.log('Before StepCount', startTimeMillis, endTimeMillis);
+    console.log('looped date', d);
     
-  //   const stepCount = await stepCountCall(TOKEN, {
-  //     startTimeMillis: startTimeMillis.getTime(),
-  //     endTimeMillis: endTimeMillis.getTime(),
-  //     durationMillis: 86400000,
-  //   })
+    const stepCount = await stepCountCall(TOKEN, {
+      startTimeMillis: startTimeMillis.getTime(),
+      endTimeMillis: endTimeMillis.getTime(),
+      durationMillis: 86400000,
+    })
 
-  //   const date = new Date(Date.UTC(
-  //     d.getUTCFullYear(),
-  //     d.getUTCMonth(),
-  //     d.getUTCDate(),
-  //     0, 0, 0, 0));
+    const date = new Date(Date.UTC(
+      d.getUTCFullYear(),
+      d.getUTCMonth(),
+      d.getUTCDate(),
+      0, 0, 0, 0));
     
-  //   const stepCountEntity = new StepCount({
-  //     user_email: email,
-  //     date,
-  //     step_count: stepCount || 0,
-  //   });
-  //   score += stepCount || 0
-  //   console.log('stepCountEntity', stepCountEntity)
-  //   await stepCountEntity.save();
-  // }
-  // return score
-  return 2
+    const stepCountEntity = new StepCount({
+      user_email: email,
+      date,
+      step_count: stepCount || 0,
+    });
+    score += stepCount || 0
+    console.log('stepCountEntity', stepCountEntity)
+    await stepCountEntity.save();
+  }
+  return score
 }
 
 const userStepCount = async (email, {startDate, endDate}) => {
